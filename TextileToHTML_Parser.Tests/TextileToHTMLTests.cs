b61ce633cc0568f47ad3eee0c2ad5883a78b1fe1
Denis Nestruev <dnestruev@syntellect.ru>
2022-03-31 18:38:34 +0300

    Промежуточное сохранение.

--- TextileToHTML_Parser.Tests/TextileToHTMLTests.cs
+++ TextileToHTML_Parser.Tests/TextileToHTMLTests.cs
@@ -13,7 +13,7 @@ namespace TextileToHTML_Parser.Tests
             var testString = "*Жирный*\r\n";
             Parser parser = new Parser(testString);
 
-            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span style=\\\"font-weight:bold;\\\">Жирный</span></p></div>\"}";
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span></span><span style=\\\"font-weight:bold;\\\">Жирный</span></span></p></div>\"}";
             var resultString = parser.GetParsedString();
 
             Assert.AreEqual(compareString, resultString);
@@ -26,7 +26,103 @@ namespace TextileToHTML_Parser.Tests
             var testString = "_Курсивный_\r\n";
             Parser parser = new Parser(testString);
 
-            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span style=\\\"font-style:italic;\\\">Курсивный</span></p></div>\"}";
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span></span><span style=\\\"font-style:italic;\\\">Курсивный</span></span></p></div>\"}";
+            var resultString = parser.GetParsedString();
+
+            Assert.AreEqual(compareString, resultString);
+        }
+
+        [TestMethod]
+        [TestCategory("Зачеркнутый текст.")]
+        public void CrossOutString()
+        {
+            var testString = "-Зачеркнутый-\r\n";
+            Parser parser = new Parser(testString);
+
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span></span><span style=\\\"text-decoration:line-through;\\\">Зачеркнутый</span></span></p></div>\"}";
+            var resultString = parser.GetParsedString();
+
+            Assert.AreEqual(compareString, resultString);
+        }
+
+        [TestMethod]
+        [TestCategory("Подчеркнутый текст.")]
+        public void UnderlineString()
+        {
+            var testString = "+Подчеркнутый+\r\n";
+            Parser parser = new Parser(testString);
+
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span></span><span style=\\\"text-decoration:underline;\\\">Подчеркнутый</span></span></p></div>\"}";
+            var resultString = parser.GetParsedString();
+
+            Assert.AreEqual(compareString, resultString);
+        }
+
+        [TestMethod]
+        [TestCategory("Жирный курсивный текст.")]
+        public void BoldItelicString()
+        {
+            var testString = "*_Жирный курсив_*\r\n";
+            Parser parser = new Parser(testString);
+
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span></span><span style=\\\"font-weight:bold;font-style:italic;\\\">Жирный курсив</em></strong></span></p></div>\"}";
+            var resultString = parser.GetParsedString();
+
+            Assert.AreEqual(compareString, resultString);
+        }
+
+        [TestMethod]
+        [TestCategory("Все стили по порядку. Каждый с новой строки.")]
+        public void AllStyleString()
+        {
+            var testString = "+Подчеркнутый+\r\n*Жирный*\r\n-Зачеркнутый-\r\n_Курсив_\r\n";
+            Parser parser = new Parser(testString);
+
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span></span><span style=\\\"text-decoration:underline;\\\">Подчеркнутый</span></span></p><p><span></span><span style=\\\"font-weight:bold;\\\">Жирный</span></span></p><p><span></span><span style=\\\"text-decoration:line-through;\\\">Зачеркнутый</span></span></p><p><span></span><span style=\\\"font-style:italic;\\\">Курсив</span></span></p></div>\"}";
+            var resultString = parser.GetParsedString();
+
+            Assert.AreEqual(compareString, resultString);
+        }
+
+        [TestMethod]
+        [TestCategory("Нумерованный список.")]
+        public void NumberedListString()
+        {
+            var testString = "# Зайдите в поле списка, например, список владельцев и выберите одного владельца.\r\n" +
+                             "# Нажмите стребку влево, чтобы переместить курсов в начало поля. \r\n" +
+                             "# Введите произвольный текст (нажмите цифру 1).\r\n" +
+                             "# Нажмите левой кнопкой мыши на выбранное в пункте 1 справочное значение.\r\n" +
+                             "# Нажмите клавишу delete для удаление элемента списка.\r\n" +
+                             "# Нажмите delete еще раз" +
+                             "\r\n" +
+                             "\r\n";
+
+            Parser parser = new Parser(testString);
+
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"></span>" +
+                                "<ol class=\\\"forum-ol\\\">" +
+                                "<li><p><span>Зайдите в поле списка, например, список владельцев и выберите одного владельца.</span></p></li>" +
+                                "<li><p><span>Нажмите стребку влево, чтобы переместить курсов в начало поля. </span></p></li>" +
+                                "<li><p><span>Введите произвольный текст (нажмите цифру 1).</span></p></li>" +
+                                "<li><p><span>Нажмите левой кнопкой мыши на выбранное в пункте 1 справочное значение.</span></p></li>" +
+                                "<li><p><span>Нажмите клавишу delete для удаление элемента списка.</span></p></li>" +
+                                "<li><p><span>Нажмите delete еще раз</span></p></li></ol></div>\"}";
+
+            var resultString = parser.GetParsedString();
+
+            Assert.AreEqual(compareString, resultString);
+        }
+
+        [TestMethod]
+        [TestCategory("С заголовком список.")]
+        public void WithHeaderString()
+        {
+            var testString = "Добавить дополнительный параметр поиска убытков по водителю (вкладка страховые данные)\r\n\r\nh1. Спецификация\r\n\r\nПараметр добавляется в общий диалог поиска.\r\nИщем на вхождение.\r\nНужно убедиться в наличии индекса на это поле.";
+
+            Parser parser = new Parser(testString);
+
+            var compareString = "{\"Text\":\"<div class=\\\"forum-div\\\"><p><span>Добавить дополнительный параметр поиска убытков по водителю (вкладка страховые данные)</span></p><p><span style=\\\"font-weight:bold;\\\" data-custom-style=\\\"font-size:18;\\\">Спецификация</span></p><p><span>Параметр добавляется в общий диалог поиска.</span></p><p><span>Ищем на вхождение.</span></p><p><span>Нужно убедиться в наличии индекса на это поле.</span></p></div>\"}";
+
             var resultString = parser.GetParsedString();
 
             Assert.AreEqual(compareString, resultString);
--- TextileToHTML_Parser/AppData/Parser.cs
+++ TextileToHTML_Parser/AppData/Parser.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace TextileToHTML_Parser.AppData
 {
@@ -21,22 +18,107 @@ namespace TextileToHTML_Parser.AppData
         private string ResultString;
 
         /// <summary>
-        /// Стандартный HTML теги.
+        /// Открытие тегов стандартного HTML.
         /// </summary>
-        private Dictionary<string, string> StandartTags = new Dictionary<string, string>();
+        private Dictionary<string, string> StandartOpeningTags = new Dictionary<string, string>();
 
         /// <summary>
-        /// HTML теги в TESSA.
+        /// Закрытие тегов стандартного HTML.
         /// </summary>
-        private Dictionary<string, string> TessaTags = new Dictionary<string, string>();
+        private Dictionary<string, string> StandartClosingTags = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Открытие тегов HTML Tessa.
+        /// </summary>
+        private Dictionary<string, string> TessaOpeningTags = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Закрытие тегов HTML Tessa.
+        /// </summary>
+        private Dictionary<string, string> TessaClosingTags = new Dictionary<string, string>();
 
         #region Tags Names
 
-        private string BoldStringName = "BoldString";
-        private string BoldStringCloseName = "BoldStringClose";
+        private readonly string BoldItalicTagName = "BoldItalicTag";
+        private readonly string BoldTagName = "BoldTag";
+        private readonly string ItalicTagName = "ItalicTag";
+        private readonly string UnderlineTagName = "UnderlineTag";
+        private readonly string CrossedOutTagName = "CrossedOutTag";
+        private readonly string NumberedListTagName = "NumberedListTag";
+        private readonly string UnNumberedListTagName = "UnNumberedListTag";
+        private readonly string ListItemTagName = "ListItemTag";
+        private readonly string ParagraphTagName = "ParagraphTag";
 
-        private string ItalicStringName = "ItalicString";
-        private string ItalicStringCloseName = "ItelicStringClose";
+        #endregion
+
+        #region Tags
+
+        private string SpanClosedTag = "</span>";
+
+        #region Standart Tags
+
+        private readonly string StandartBoldItalicTag = "<strong><em>";
+        private readonly string StandartBoldTag = "<strong>";
+        private readonly string StandartItalicTag = "<em>";
+        private readonly string StandartUnderlineTag = "<ins>";
+        private readonly string StandartCrossedOutTag = "<del>";
+        private readonly string StandartNumberedListTag = "<ol>";
+        private readonly string StandartUnNumberedListTag = "<ul>";
+        private readonly string StandartListItemTag = "<li>";
+        private readonly string StandartParagraphTag = "<p>";
+
+        private readonly string StandartBoldItalicClosedTag = "</strong></em>";
+        private readonly string StandartBoldClosedTag = "</strong>";
+        private readonly string StandartItalicClosedTag = "</em>";
+        private readonly string StandartUnderlineClosedTag = "</ins>";
+        private readonly string StandartCrossedOutClosedTag = "</del>";
+        private readonly string StandartNumberedListClosedTag = "</ol>";
+        private readonly string StandartUnNumberedListClosedTag = "</ul>";
+        private readonly string StandartListItemClosedTag = "</li>";
+        private readonly string StandartParagraptClosedTag = "</p>";
+
+        private string StandartNewLineTag = "<br />";
+
+        #endregion
+
+        #region Tessa Tags
+
+        private readonly string TessaBoldItalicTag = "</span><span style=\\\"font-weight:bold;font-style:italic;\\\">";
+        private readonly string TessaBoldTag = "</span><span style=\\\"font-weight:bold;\\\">";
+        private readonly string TessaItalicTag = "</span><span style=\\\"font-style:italic;\\\">";
+        private readonly string TessaUnderlineTag = "</span><span style=\\\"text-decoration:underline;\\\">";
+        private readonly string TessaCrossedOutTag = "</span><span style=\\\"text-decoration:line-through;\\\">";
+        private readonly string TessaNumberedListTag = "</span><ol class=\\\"forum-ol\\\">";
+        private readonly string TessaUnNumberedListTag = "</span><ul class=\\\"forum-ul\\\">";
+        private readonly string TessaListItemTag = "<li><p><span>";
+        private readonly string TessaParagraphTag = "<p><span>";
+
+        private readonly string TessaListItemClosedTag = "</span></p></li>";
+        private readonly string TessaParagraphClosedTag = "</span></p>";
+
+        private readonly string TessaNewLineTag = "</span></p><p><span>";
+
+        #endregion
+
+        #endregion
+
+        #region Regex Templates
+
+        /// <summary>
+        /// Шаблон регулярного выражения открытия тега заголовка.
+        /// </summary>
+        private static readonly string headerOpenTagTemplate = @"<h[1-6]>";
+
+        /// <summary>
+        /// Шаблон регулярного выражения открытия тега заголовка.
+        /// </summary>
+        private static readonly string headerCloseTagTemplate = @"<\/h[1-6]>";
+
+        private static readonly Regex _headerOpenTag = new Regex(headerOpenTagTemplate,
+           RegexOptions.Singleline | RegexOptions.Compiled);
+
+        private static readonly Regex _headerCloseTag = new Regex(headerCloseTagTemplate,
+           RegexOptions.Singleline | RegexOptions.Compiled);
 
         #endregion
 
@@ -60,11 +142,7 @@ namespace TextileToHTML_Parser.AppData
 
         public string GetParsedString()
         {
-            this.ParseBoldTags();
-            this.ParseItelicTags();
-
-            this.SetPreAndPostString();
-            this.RemoveSumbolNewString();
+            this.ParseString();
 
             return ResultString;
         }
@@ -75,54 +153,109 @@ namespace TextileToHTML_Parser.AppData
 
         #region Initial Methods
 
+        /// <summary>
+        /// Инициализация стандартных HTML тегов.
+        /// </summary>
         private void InitialStandartTags()
         {
-            StandartTags.Clear();
-            StandartTags.Add(BoldStringName, "<strong>");
-            StandartTags.Add(BoldStringCloseName, "</strong>");
-            StandartTags.Add(ItalicStringName, "<em>");
-            StandartTags.Add(ItalicStringCloseName, "</em>");
+            StandartOpeningTags.Clear();
+            StandartClosingTags.Clear();
+
+            // тут важен порядок добавления.
+            StandartOpeningTags.Add(ParagraphTagName, StandartParagraphTag);
+            StandartOpeningTags.Add(BoldItalicTagName, StandartBoldItalicTag);
+            StandartOpeningTags.Add(BoldTagName, StandartBoldTag);
+            StandartOpeningTags.Add(ItalicTagName, StandartItalicTag);
+            StandartOpeningTags.Add(UnderlineTagName, StandartUnderlineTag);
+            StandartOpeningTags.Add(CrossedOutTagName, StandartCrossedOutTag);
+            StandartOpeningTags.Add(NumberedListTagName, StandartNumberedListTag);
+            StandartOpeningTags.Add(UnNumberedListTagName, StandartUnNumberedListTag);
+            StandartOpeningTags.Add(ListItemTagName, StandartListItemTag);
+
+            StandartClosingTags.Add(ParagraphTagName, StandartParagraptClosedTag);
+            StandartClosingTags.Add(BoldItalicTagName, StandartBoldItalicClosedTag);
+            StandartClosingTags.Add(BoldTagName, StandartBoldClosedTag);
+            StandartClosingTags.Add(ItalicTagName, StandartItalicClosedTag);
+            StandartClosingTags.Add(UnderlineTagName, StandartUnderlineClosedTag);
+            StandartClosingTags.Add(CrossedOutTagName, StandartCrossedOutClosedTag);
+            StandartClosingTags.Add(NumberedListTagName, StandartNumberedListClosedTag);
+            StandartClosingTags.Add(UnNumberedListTagName, StandartUnNumberedListClosedTag);
+            StandartClosingTags.Add(ListItemTagName, StandartListItemClosedTag);
         }
 
+        /// <summary>
+        /// Инициализация HTML тегов TESSA.
+        /// </summary>
         private void InitialTessaTags()
         {
-            TessaTags.Clear();
-            TessaTags.Add(BoldStringName, "<span style=\\\"font-weight:bold;\\\">");
-            TessaTags.Add(BoldStringCloseName, "</span>");
-            TessaTags.Add(ItalicStringName, "<span style=\\\"font-style:italic;\\\">");
-            TessaTags.Add(ItalicStringCloseName, "</span>");
+            TessaOpeningTags.Clear();
+            TessaClosingTags.Clear();
+
+            TessaOpeningTags.Add(ParagraphTagName, TessaParagraphTag);
+            TessaOpeningTags.Add(BoldItalicTagName, TessaBoldItalicTag);
+            TessaOpeningTags.Add(BoldTagName, TessaBoldTag);
+            TessaOpeningTags.Add(ItalicTagName, TessaItalicTag);
+            TessaOpeningTags.Add(UnderlineTagName, TessaUnderlineTag);
+            TessaOpeningTags.Add(CrossedOutTagName, TessaCrossedOutTag);
+            TessaOpeningTags.Add(NumberedListTagName, TessaNumberedListTag);
+            TessaOpeningTags.Add(UnNumberedListTagName, TessaUnNumberedListTag);
+            TessaOpeningTags.Add(ListItemTagName, TessaListItemTag);
+
+            TessaClosingTags.Add(ParagraphTagName, TessaParagraphClosedTag);
+            TessaClosingTags.Add(BoldItalicTagName, SpanClosedTag);
+            TessaClosingTags.Add(BoldTagName, SpanClosedTag);
+            TessaClosingTags.Add(ItalicTagName, SpanClosedTag);
+            TessaClosingTags.Add(UnderlineTagName, SpanClosedTag);
+            TessaClosingTags.Add(CrossedOutTagName, SpanClosedTag);
+            TessaClosingTags.Add(NumberedListTagName, StandartNumberedListClosedTag);
+            TessaClosingTags.Add(UnNumberedListTagName, StandartUnNumberedListClosedTag);
+            TessaClosingTags.Add(ListItemTagName, TessaListItemClosedTag);
         }
 
         #endregion
 
-        /// <summary>
-        /// Обработка жирного текста.
-        /// Обрабтка тега <strong>.
-        /// </summary>
-        private void ParseBoldTags()
+        private void ParseString()
         {
-            string tagOpenName = BoldStringName;
-            if (ResultString.Contains(StandartTags[tagOpenName]))
+            foreach(var tag in TessaOpeningTags)
             {
-                string tagCloseName = BoldStringCloseName;
-                ResultString = ResultString.Replace(StandartTags[tagOpenName], TessaTags[tagOpenName]);
-                ResultString = ResultString.Replace(StandartTags[tagCloseName], TessaTags[tagCloseName]);
+                this.ParseTag(tag);
+            }
+
+            // установка начала и конца строки.
+            this.SetPreAndPostString();
+            // удаление лишних символов новой строки.
+            this.RemoveSumbolNewString();
+            // преобразуем тег <br /> в </p><p>.
+            this.ParseNewLineTag();
+            // преобразуем код "&#8220" и "&#8221" в символы "\\\"".
+            this.ParseQuotesSymbol();
+
+            this.ParseHeaderString();
+        }
+
+        /// <summary>
+        /// Заменяет тег стандартного HTML на тег TESSA HTML.
+        /// </summary>
+        /// <param name="tag">Ключ и значение стандартного HTML тега.</param>
+        private void ParseTag(KeyValuePair<string, string> tag)
+        {
+            string tagName = tag.Key;
+            if (ResultString.Contains(StandartOpeningTags[tag.Key]))
+            {
+                ResultString = ResultString.Replace(StandartOpeningTags[tagName], TessaOpeningTags[tagName]);
+                ResultString = ResultString.Replace(StandartClosingTags[tagName], TessaClosingTags[tagName]);
             }
         }
 
         /// <summary>
-        /// Обработка курсивного текста.
-        /// Обрабтка тега <em>.
+        /// Преобразование тега новой строки из стандартного HTML в TESSA HTML.
         /// </summary>
-        private void ParseItelicTags()
+        private void ParseNewLineTag()
         {
-            string tagOpenName = ItalicStringName;
-            if (ResultString.Contains(StandartTags[tagOpenName]))
+            if(ResultString.Contains(StandartNewLineTag))
             {
-                string tagCloseName = ItalicStringCloseName;
-                ResultString = ResultString.Replace(StandartTags[tagOpenName], TessaTags[tagOpenName]);
-                ResultString = ResultString.Replace(StandartTags[tagCloseName], TessaTags[tagCloseName]);
-            }    
+                ResultString = ResultString.Replace(StandartNewLineTag, TessaNewLineTag);
+            }
         }
 
         /// <summary>
@@ -155,6 +288,30 @@ namespace TextileToHTML_Parser.AppData
             {
                 ResultString = ResultString.Remove(symbolIndex, 1);
                 symbolIndex = ResultString.IndexOf('\n');
+            }
+        }
+
+        /// <summary>
+        /// Преобразовать код "&#8220" и "&#8221" в символы "\\\"".
+        /// </summary>
+        private void ParseQuotesSymbol()
+        {
+            ResultString = ResultString.Replace("&#8220;", "\\\"");
+            ResultString = ResultString.Replace("&#8221;", "\\\"");
+        }
+
+        /// <summary>
+        /// Преобразование тега <h[1-6]> в жирный 18 шрифт Tessa.
+        /// </summary>
+        private void ParseHeaderString()
+        {
+            while (Regex.IsMatch(ResultString, headerOpenTagTemplate, RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline | RegexOptions.Compiled))
+            {
+                ResultString = _headerOpenTag.Replace(ResultString, "<p><span style=\\\"font-weight:bold;\\\" data-custom-style=\\\"font-size:18;\\\">");
+            }
+            while(Regex.IsMatch(ResultString, headerCloseTagTemplate, RegexOptions.IgnorePatternWhitespace | RegexOptions.Singleline | RegexOptions.Compiled))
+            {
+                ResultString = _headerCloseTag.Replace(ResultString, "</span></p>");
             }
         }
 
